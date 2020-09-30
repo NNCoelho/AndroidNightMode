4941e0a907d90e54f56d38df38a48ceba689262c
NNCoelho <coelho301181@gmail.com>
2020-09-30 08:41:50 +0100

    Initial commit

---
+++ app/src/main/java/com/example/androidnightmode/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.androidnightmode;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        @SuppressLint("UseSwitchCompatOrMaterialCode")
+        Switch switchNightMode = findViewById(R.id.switchNightMode);
+        TextView modeName = findViewById(R.id.modeName);
+        TextView themeName = findViewById(R.id.themeName);
+
+        int currentMode = AppCompatDelegate.getDefaultNightMode();
+
+        if (currentMode == AppCompatDelegate.MODE_NIGHT_YES) {
+            switchNightMode.setChecked(true);
+            themeName.setText(R.string.night_theme_title);
+            modeName.setText(R.string.day_switch);
+        } else {
+            switchNightMode.setChecked(false);
+            themeName.setText(R.string.day_theme_title);
+            modeName.setText(R.string.night_switch);
+        }
+
+        switchNightMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    restartCurrentActivity();
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    restartCurrentActivity();
+                }
+            }
+        });
+    }
+
+    private void restartCurrentActivity() {
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+    }
+}
